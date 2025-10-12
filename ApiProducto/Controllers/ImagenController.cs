66f090a7813306dde9429fe548a1b281e3a8e320
julianfparodi <julianfacundoparodi@gmail.com>
2025-10-12 19:19:22 -0300

    Post Imagenes

---
+++ ApiProducto/Controllers/ImagenController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using dominio;
+using negocio;
+using ApiProducto.Models;
+
+namespace ApiProducto.Controllers
+{
+    public class ImagenController : ApiController
+    {
+
+
+        // POST: api/Imagen
+        public void Post([FromBody]ImagenDto imagen)
+        {
+           
+                ImagenNegocio negocio = new ImagenNegocio();
+
+                foreach (var url in imagen.Imagenes)
+                {
+                    Imagen nueva = new Imagen
+                    {
+                        IdArticulo = imagen.Id,
+                        ImagenUrl = url
+                    };
+                    negocio.agregar(nueva);
+                }
+        }
+    }
+}
---
+++ ApiProducto/Models/ImagenDto.cs
@@ -0,0 +1,15 @@
+﻿using dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApiProducto.Models
+{
+    public class ImagenDto
+    {
+
+        public int Id { get; set; }
+        public List<string> Imagenes { get; set; }
+    }
+}
